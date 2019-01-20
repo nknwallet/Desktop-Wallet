8199858ccad65ee5261c932f2f3d3c417500f222
Matin Kaboli <matinkaboli@aol.com>
2019-01-20 16:17:24 +0330

    feat: complete background component

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import logo from 'Root/images/logo.png';
+import logoWithName from 'Root/images/LogoWithName.png';
+
 import styles from './styles.less';
 
 function Background() {
@@ -7,6 +10,17 @@ function Background() {
     <div className={styles.background}>
       <p className={styles.nknName}>NKN</p>
       <p className={styles.walletName}>Wallet</p>
+      <div className={styles.details}>
+        Open Source
+        <div className={styles.circle} />
+        Fast
+        <div className={styles.circle} />
+        Safe
+      </div>
+
+      <div src={logo} alt="Logo" className={styles.logo} />
+
+      <img src={logoWithName} alt="Logo" className={styles.logoWithName} />
     </div>
   );
 }

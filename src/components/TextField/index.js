bf2e7941138340255b3acf28128e2038a39758f9
Matin Kaboli <matinkaboli@aol.com>
2019-01-22 13:13:29 +0330

    feat: add TextField input

---
+++ src/components/TextField/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './styles.less';
+
+function TextField() {
+  return (
+    <input className={styles.input} />
+  );
+}
+
+export default TextField;

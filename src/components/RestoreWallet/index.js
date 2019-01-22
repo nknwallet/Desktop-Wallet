d62bd3691055bcb814ab64e41bd0212d04bf4ece
Matin Kaboli <matinkaboli@aol.com>
2019-01-22 13:12:16 +0330

    feat: add restoreWallet component

---
+++ src/components/RestoreWallet/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './styles.less';
+
+function RestoreWallet() {
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>RESTORE WALLET</p>
+      <p className={styles.details}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      </p>
+      <p className={styles.label}>Wallet name</p>
+    </div>
+  );
+}
+
+export default RestoreWallet;

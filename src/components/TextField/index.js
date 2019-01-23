f997821869675c35f1a7009ec622fe8df57e4e69
Matin Kaboli <matinkaboli@aol.com>
2019-01-23 16:43:59 +0330

    feat: add show/hide password property for textField component

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import eyeOn from 'Root/images/eye-on.svg';
+import eyeOff from 'Root/images/eye-off.svg';
 
 import styles from './styles.less';
 
-function TextField(props) {
-  return (
-    <div className={styles.container}>
-      <input
-        className={styles.input}
-        placeholder={props.placeholder}
-      />
+class TextField extends Component {
+  state = {
+    eyeImg: eyeOn,
+    type: this.props.type,
+    eye: this.props.type === 'password' ? 'on' : 'off',
+  };
 
-      {props.invisible && <span className={styles.invisible}>aa</span>}
-    </div>
-  );
+  handleClick = () => {
+    this.setState(state => ({
+      eye: state.eye === 'on' ? 'off' : 'on',
+      eyeImg: state.eye === 'on' ? eyeOff : eyeOn,
+      type: state.type === 'password' ? 'text' : 'password',
+    }));
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <input
+          type={this.state.type}
+          className={styles.input}
+          placeholder={this.props.placeholder}
+        />
+
+        {this.props.type === 'password'
+          && (
+            <span className={styles.invisible}>
+              <img
+                alt="Eye"
+                src={this.state.eyeImg}
+                onClick={this.handleClick}
+              />
+            </span>
+          )
+      }
+      </div>
+    );
+  }
 }
 
 export default TextField;

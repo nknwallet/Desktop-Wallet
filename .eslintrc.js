4eb3b023cceba12cdc35fba01d98ebf816386ec0
Matin Kaboli <matinkaboli@aol.com>
2019-01-23 14:24:01 +0330

    chore: ignore react/prop-types from eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
 	extends: 'airbnb',
 	parser: 'babel-eslint',
 	rules: {
+		'react/prop-types': 0,
 		'import/no-unresolved': 0,
 		'no-restricted-syntax': 0,
 		'react/forbid-prop-types': 0,

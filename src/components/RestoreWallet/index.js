f62645c1215906d7082028beb63f29c85f987c04
Matin Kaboli <matinkaboli@aol.com>
2019-01-23 16:10:42 +0330

    feat: add RESTORe button

--- src/components/RestoreWallet/index.js
+++ src/components/RestoreWallet/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Button from 'Root/components/Button';
+import TextField from 'Root/components/TextField';
 
 import styles from './styles.less';
 
@@ -10,6 +14,25 @@ function RestoreWallet() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
       </p>
       <p className={styles.label}>Wallet name</p>
+      <TextField placeholder="8-20 characters, default to MyWallet" />
+
+      <p className={styles.label}>Private Key</p>
+      <TextField placeholder="Your private key" />
+
+      <p className={styles.label}>Password</p>
+      <TextField placeholder="8-20 characters" invisible />
+
+      <p className={styles.label}>Confirm Password</p>
+      <TextField placeholder="8-20 characters" invisible />
+
+      <div className={styles.links}>
+        <Link to="/restore-private">Restore from private key!</Link>
+        <Link to="/create-wallet">Create Wallet</Link>
+      </div>
+
+      <Button>
+        RESTORE
+      </Button>
     </div>
   );
 }

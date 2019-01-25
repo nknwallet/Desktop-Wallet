880ff807fb03e3a3beb5ef2e25c6addb0b1d2002
Matin Kaboli <matinkaboli@aol.com>
2019-01-25 21:23:53 +0330

    feat: add dashboard component

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './styles.less';
+
+function Dashboard() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.navBar}>
+        <p>Block: 000</p>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;

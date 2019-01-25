8c966e5c29e161b2e04de47cf672ec3a651975a3
Matin Kaboli <matinkaboli@aol.com>
2019-01-25 21:57:05 +0330

    refactor: add tools and pages into different folders

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Background from './Background';
+import Dashboard from './pages/Dashboard';
+import OpenWallet from './pages/OpenWallet';
+import CreateWallet from './pages/CreateWallet';
+import RestoreWallet from './pages/RestoreWallet';
 
 function App() {
   return (
-    <div>
-      <Background />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/open-wallet" component={OpenWallet} />
+        <Route exact path="/create-wallet" component={CreateWallet} />
+        <Route exact path="/restore-wallet" component={RestoreWallet} />
+        <Route component={RestoreWallet} />
+      </Switch>
+    </Router>
   );
 }
 

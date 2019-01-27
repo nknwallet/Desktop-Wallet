da9be0b671205da2bfd2454e01721e0bb317c18e
Matin Kaboli <matinkaboli@aol.com>
2019-01-27 17:49:08 +0330

    refactor: use Box component

--- src/components/pages/Dashboard/index.js
+++ src/components/pages/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Chart from 'Root/components/tools/Chart';
+import Box from 'Root/components/tools/Box';
 
 import styles from './styles.less';
 
@@ -9,7 +9,22 @@ function Dashboard() {
     <div className={styles.container}>
       <div className={styles.navBar}>
         <p>Block: 000</p>
-        <Chart />
+      </div>
+
+      <div className={styles.info}>
+        <div className={styles.column1}>
+          <Box title="Sales" width={891} height={519} line />
+
+          <Box title="Last Transaction" width={890} height={317} />
+        </div>
+
+        <div className={styles.column2}>
+          <Box width={394} height={119} />
+
+          <Box title="Account" width={395} height={383} line />
+
+          <Box title="Title" width={398} height={324} line />
+        </div>
       </div>
     </div>
   );

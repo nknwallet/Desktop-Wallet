5fb11c62f3c737b1ca168d1deff6beed27e437ff
Matin Kaboli <matinkaboli@aol.com>
2019-02-08 00:40:48 +0330

    refactor: move checkToken from user/ to wallet/

---
+++ src/actions/wallet/checkToken.js
@@ -0,0 +1,17 @@
+import store from 'Root/store';
+import types from 'Root/actions';
+
+export default async () => new Promise((resolve) => {
+  const token = global.localStorage.getItem('token');
+
+  if (token !== null) {
+    store.dispatch({
+      token,
+      type: types.token.LOGIN,
+    });
+
+    return resolve();
+  }
+
+  return resolve();
+});

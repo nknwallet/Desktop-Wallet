b7739dac9acf6510f2482e4d42ec743f8d0f8520
Matin Kaboli <matinkaboli@aol.com>
2019-02-11 02:10:12 +0330

    feat: create page component

---
+++ src/components/tools/Page/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Button from 'Root/components/tools/Button';
+import Background from 'Root/components/pages/Background';
+
+import styles from './styles.less';
+
+function Page(props) {
+  return (
+    <div style={{ display: 'flex' }}>
+      <Background />
+
+      <div className={styles.container}>
+        <form action="/" method="POST" onSubmit={props.handleSubmit}>
+          <p className={styles.title}>{props.title}</p>
+
+          {props.children}
+
+          <div className={styles.links}>
+            {props.links}
+          </div>
+
+          <Button type="submit">
+            {props.buttonTitle}
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Page;

9c500c0a9e413b8d50c1af98908e59a2ffa18b5a
Matin Kaboli <matinkaboli@aol.com>
2019-02-12 20:57:05 +0330

    feat: add download function

---
+++ src/helpers/download.js
@@ -0,0 +1,17 @@
+export default (filename, text) => {
+  const element = global.document.createElement('a');
+
+  element.setAttribute(
+    'href',
+    `data:application/json;charset=utf-8,${text}`,
+  );
+
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+
+  global.document.body.appendChild(element);
+
+  element.click();
+
+  global.document.body.removeChild(element);
+};

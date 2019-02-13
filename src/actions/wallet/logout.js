e812a27f1ae55654a5012ffef523583f70b1c449
Matin Kaboli <matinkaboli@aol.com>
2019-02-14 02:48:25 +0330

    feat: add logout file

---
+++ src/actions/wallet/logout.js
@@ -0,0 +1,18 @@
+import store from 'Root/store';
+import types from 'Root/actions';
+
+export default async ({ push }) => {
+  store.dispatch({
+    type: types.token.LOGOUT,
+  });
+
+  store.dispatch({
+    type: types.wallet.LOGOUT,
+  });
+
+  global.localStorage.removeItem('name');
+  global.localStorage.removeItem('password');
+  global.localStorage.removeItem('privateKey');
+
+  push('/restore-wallet');
+};

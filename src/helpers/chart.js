40bc1172946efb2da4ab51a6084b0886efa672e2
Matin Kaboli <matinkaboli@aol.com>
2019-02-19 16:20:38 +0330

    chore: send a request to get the data for the chart

---
+++ src/helpers/chart.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+export default () => new Promise((resolve, reject) => {
+  const now = Date.now();
+  const week = 1000 * 60 * 60 * 24 * 7;
+  let link = 'https://graphs2.coinmarketcap.com/currencies/nkn';
+
+  link += `/${now - week}/${now}`;
+
+  fetch(link)
+    .then(res => res.json())
+    .then((data) => {
+      resolve(data);
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});

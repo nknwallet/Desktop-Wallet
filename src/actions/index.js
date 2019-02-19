e4d80068e62dcb38543db3cd5438bb0ec0215a5d
Matin Kaboli <matinkaboli@aol.com>
2019-02-19 16:22:51 +0330

    chore: add action names

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,10 @@ export default {
     OPEN: 'wallet/OPEN',
     LOGOUT: 'wallet/LOGOUT',
     CREATE: 'wallet/CREATE',
+    QRCODE: 'wallet/QRCODE',
     RESTORE: 'wallet/RESTORE',
+    BALANCE: 'wallet/BALANCE',
+    PRICE_USD: 'wallet/PRICE_USD',
   },
   modal: {
     OPEN: 'modal/OPEN',

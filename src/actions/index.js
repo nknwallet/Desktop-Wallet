351d9193297917c17b3237a9aa3bf0e888f3a9e8
Matin Kaboli <matinkaboli@aol.com>
2019-02-20 19:58:20 +0330

    fix: add action names

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,12 +5,14 @@ export default {
   },
   wallet: {
     OPEN: 'wallet/OPEN',
+    BLOCK: 'wallet/BLOCK',
     LOGOUT: 'wallet/LOGOUT',
     CREATE: 'wallet/CREATE',
     QRCODE: 'wallet/QRCODE',
     RESTORE: 'wallet/RESTORE',
     BALANCE: 'wallet/BALANCE',
     PRICE_USD: 'wallet/PRICE_USD',
+    TRANSACTIONS: 'wallet/TRANSACTIONS',
   },
   modal: {
     OPEN: 'modal/OPEN',
--- src/actions/wallet/loadDashboard.js
+++ src/actions/wallet/loadDashboard.js
@@ -1,12 +1,20 @@
+import price from 'Root/helpers/price';
 import chart from './chart';
+import block from './blocks';
 import qrcode from './qrcode';
 import balance from './balance';
+import transactions from './transactions';
 
 export default async ({ push }) => {
   try {
-    await chart();
-    await qrcode();
-    await balance();
+    await Promise.all([
+      chart(),
+      block(),
+      price(),
+      qrcode(),
+      balance(),
+      transactions(),
+    ]);
 
     push('/dashboard');
   } catch (error) {

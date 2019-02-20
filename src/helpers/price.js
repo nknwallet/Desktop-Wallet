e000eef667870de9d9c41d09854a74010f7ed5dd
Matin Kaboli <matinkaboli@aol.com>
2019-02-20 19:53:45 +0330

    chore: get the NKN price helper

---
+++ src/helpers/price.js
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch';
+
+export default async () => new Promise((resolve, reject) => {
+  resolve()
+  // fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=NKN', {
+  //   headers: {
+  //     'X-CMC_PRO_API_KEY': '51f9d377-94b9-4757-83bd-51b65471337d',
+  //   },
+  // })
+  //   .then(res => res.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //   });
+});

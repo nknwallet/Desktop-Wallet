ff29500fe5ad57dac5c6afdeb680c39f7da3188e
Matin Kaboli <matinkaboli@aol.com>
2019-02-21 14:05:52 +0330

    fix: connect Sales box to redux and show the price

--- src/components/pages/Dashboard/Boxes/Sales/index.js
+++ src/components/pages/Dashboard/Boxes/Sales/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Box from 'Root/components/tools/Box';
 import Chart from 'Root/components/tools/Chart';
 
 import styles from './styles.less';
 
-function Sales() {
+function Sales(props) {
   return (
     <Box title="Sales" width={891} height={519} line>
       <div className={styles.container}>
@@ -13,11 +14,16 @@ function Sales() {
           <p>$</p>
         </div>
 
-        <p className={styles.price}>$185.415</p>
+        <p className={styles.price}>
+          $
+          {props.wallet.price}
+        </p>
       </div>
       <Chart />
     </Box>
   );
 }
 
-export default Sales;
+export default connect(state => ({
+  wallet: state.wallet,
+}))(Sales);

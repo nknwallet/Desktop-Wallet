3d94750c81d35a92a953054d32348416ed483cce
Matin Kaboli <matinkaboli@aol.com>
2019-03-13 18:49:32 +0330

    refactor: change refresh loader (remove qrCode)

--- src/actions/wallet/refresh.js
+++ src/actions/wallet/refresh.js
@@ -1,7 +1,23 @@
-import loadDashboard from './loadDashboard';
+import price from './price';
+import chart from './chart';
+import block from './blocks';
+import balance from './balance';
+import transactions from './transactions';
 
 export default async ({ push }) => {
   push('/loading');
 
-  loadDashboard();
+  try {
+    await Promise.all([
+      chart(),
+      block(),
+      price(),
+      balance(),
+      transactions(),
+    ]);
+
+    push('/dashboard');
+  } catch (error) {
+    console.log(error);
+  }
 };

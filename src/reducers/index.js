11d568b23cab2f8b3ab4708e0ee4de82489b34ed
Matin Kaboli <matinkaboli@aol.com>
2019-03-16 15:43:36 +0330

    fix: add language reducer to the main reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import token from './token';
 import modal from './modal';
 import wallet from './wallet';
 import internet from './internet';
+import language from './language';
 
 export default combineReducers({
   token,
   modal,
   wallet,
   internet,
+  language,
 });

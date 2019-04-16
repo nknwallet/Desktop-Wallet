f762507924bde2ad855ed6998f0e9165c78b8198
Matin Kaboli <matinkaboli@aol.com>
2019-04-17 02:33:34 +0430

    fix: remove the watch property

--- webpack/dev.js
+++ webpack/dev.js
@@ -3,7 +3,7 @@ const common = require('./common');
 
 module.exports = Object.assign(common, {
   mode: 'development',
-  watch: true,
+  // watch: true,
   plugins: [
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'development',

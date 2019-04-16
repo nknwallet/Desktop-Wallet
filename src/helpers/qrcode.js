fe288fda3bbabb8d72185bc7f7a98165e4a73b4e
Matin Kaboli <matinkaboli@aol.com>
2019-04-17 02:48:11 +0430

    refactor: use qrcode package

--- src/helpers/qrcode.js
+++ src/helpers/qrcode.js
@@ -1,29 +1,8 @@
-import fetch from 'node-fetch';
+import qrcode from 'qrcode';
 
-function arrayBufferToBase64(buffer) {
-  let binary = '';
-  const bytes = [].slice.call(new Uint8Array(buffer));
-
-  bytes.forEach((b) => {
-    binary += String.fromCharCode(b);
-  });
-
-  return global.btoa(binary);
-}
-
-export default string => new Promise((resolve, reject) => {
-  fetch(
-    `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${string}`,
-  )
-    .then((res) => {
-      res.arrayBuffer().then((buffer) => {
-        const base64Flag = 'data:image/png;base64,';
-        const imageStr = arrayBufferToBase64(buffer);
-
-        resolve(base64Flag + imageStr);
-      });
-    })
-    .catch((error) => {
-      reject(error);
+export default string => new Promise((resolve) => {
+  qrcode.toDataURL(string)
+    .then((url) => {
+      resolve(url);
     });
 });
